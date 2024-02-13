f018bce2d1202b9854518eef6e4d5e92bc5d698f
Tetrodomino <wettrimotto@naver.com>
2024-02-13 12:39:17 +0900

    first commit 2024-02-13

---
+++ src/main/java/ch05/Ex01_FirstSevlet.java
@@ -0,0 +1,41 @@
+package ch05;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Servlet implementation class Ex01_FirstSevlet
+ */
+@WebServlet("/hello")
+public class Ex01_FirstSevlet extends HttpServlet {
+	// Get 방식의 요청이 왔을 때 처리하는 코드
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String html = "<!DOCTYPE html>"
+				+ "<html>"
+				+ "<head>"
+				+ "<meta charset=\"UTF-8\">"
+				+ "<title>첫 번째 서블릿Sevlet</title>"
+				+ "</head>"
+				+ "<body>"
+				+ "	<h1>자바 서블릿에서 작성한 코드</h1>"
+				+ "</body>"
+				+ "</html>";
+		response.setContentType("text/html; charset=utf-8");
+		PrintWriter out = response.getWriter();
+		out.print(html);
+	}
+
+	/**
+	 *	Post 요청이 왔을 때 처리하는 코드
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
